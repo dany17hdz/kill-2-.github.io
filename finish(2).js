2e9abb06955bed653c1cedb398e44e1aa5b3e959
dany17hdz <danielhernandezhernandez142@gmail.com>
2025-12-11 18:38:27 -0600

    Update finish(2).js

--- finish(2).js
+++ finish(2).js
@@ -2,10 +2,11 @@ var llave = document.querySelector("input")
 var butt = document.querySelector("button");
 
 butt.addEventListener("click", ()=>{
-    if(llave.value==="explorador"){
+    if(llave.value ==="explorador"){
         window.location.href="finish(3).html"
     }else{
         alert("no fue posible !!incorrecto!!")
     }
 
 })
+
