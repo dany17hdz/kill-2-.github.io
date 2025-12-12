5710c032660653a2c64d2f1a4ffbabc5cc215d91
dany17hdz <danielhernandezhernandez142@gmail.com>
2025-12-11 18:36:59 -0600

    Update finish(3).js

--- finish(3).js
+++ finish(3).js
@@ -1,8 +1,9 @@
 function mostrar(){
     var r = document.getElementById("res").value.trim().toLowerCase();
-    if(r === "aristoteles"){
+    if(r.value === "aristoteles"){
         window.location.href="finish(4).html"
     } else {
         alert("incorrecto")
     }
+
 }
